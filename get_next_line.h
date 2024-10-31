2b990a68d9c7ce87906754ff093218e04aeb59c9
julienhanse <shibaz.contact@gmail.com>
2024-10-31 23:31:45 +0100

    initialize project

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 23:26:05 by juhanse           #+#    #+#             */
+/*   Updated: 2024/10/31 23:30:37 by juhanse          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_GNL_H
+# define FT_GNL_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+# define BUFFER_SIZE 5
+
+char	*get_next_line(int fd);
+
+#endif
