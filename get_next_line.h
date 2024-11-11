949f509bace7f964dedf054f311d9c9695db156a
julienhanse <shibaz.contact@gmail.com>
2024-11-12 00:49:06 +0100

    add: declaration functions

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 23:26:05 by juhanse           #+#    #+#             */
-/*   Updated: 2024/10/31 23:30:37 by juhanse          ###   ########.fr       */
+/*   Updated: 2024/11/12 00:47:21 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,14 @@
 
 # include <unistd.h>
 # include <stdlib.h>
-# include <stdio.h>
 
 # define BUFFER_SIZE 5
+
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 char	*get_next_line(int fd);
 
