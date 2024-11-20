df9fe1961f714e3ccc5fb58ec0847ce77d1d9369
Julien Hanse <julien.hanse12@gmail.com>
2024-11-20 15:22:12 +0100

    test: tuto gnl

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 23:26:05 by juhanse           #+#    #+#             */
-/*   Updated: 2024/11/20 12:51:25 by juhanse          ###   ########.fr       */
+/*   Updated: 2024/11/20 14:53:08 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,18 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1
+#  define BUFFER_SIZE 42
 # endif
 
+# include <stdio.h> // REMOVE
 # include <unistd.h>
 # include <stdlib.h>
+
+typedef struct s_list
+{
+	char			*content;
+	struct s_list	*next;
+}	t_list;
 
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s);
---
+++ main.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 14:43:46 by juhanse           #+#    #+#             */
+/*   Updated: 2024/11/20 14:46:05 by juhanse          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	fd = open("test/simple", 0_RDONLY);
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break;
+		printf("%s", line);
+		free(line);
+	}
+	return (0);
+}
