a12735d802d3ee38ce74f0eb63717cc6f95df7b4
juhanse <julien.hanse12@gmail.com>
2024-11-27 14:31:10 +0100

    add: additional tests

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: juhanse <juhanse@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 14:43:46 by juhanse           #+#    #+#             */
-/*   Updated: 2024/11/27 13:50:36 by juhanse          ###   ########.fr       */
+/*   Updated: 2024/11/27 14:30:24 by juhanse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,24 @@ int	main(void)
 {
 	int		fd;
 	char	*line;
+	char	*a;
+	char	*empty;
+	char	*b;
 
 	fd = open("test.txt", O_RDONLY);
-	while (1)
+	a = get_next_line(fd);
+	empty = get_next_line(fd);
+	b = get_next_line(fd);
+	if (!a || !empty || !b)
+		return (0);
+	printf("%s%s%s", a, empty, b);
+/* 	while (1)
 	{
 		line = get_next_line(fd);
 		if (!line)
 			break ;
 		printf("%s", line);
 		free(line);
-	}
+	} */
 	return (0);
 }
